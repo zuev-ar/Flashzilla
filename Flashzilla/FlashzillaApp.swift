3e7c784e375110557dfd199b337d696ff1fe975e
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-09-08 18:08:44 +0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Arkasha Zuev on 08.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
