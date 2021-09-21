a73f7214d0a168371d11127d831417bb654bacc2
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-09-21 19:11:50 +0500

    project 18, part 4, Coloring views as we swipe

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,6 +9,9 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
     @State private var cards = [Card](repeating: Card.example, count: 10)
     
     var body: some View {
@@ -26,6 +29,26 @@ struct ContentView: View {
                         }
                         .stacked(at: index, in: self.cards.count)
                     }
+                }
+            }
+            
+            if differentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Image(systemName: "xmark:circl")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                        clipShape(Circle())
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
                 }
             }
         }
